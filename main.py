ada4748476e51e1966068c15d1332bae1d57fe1f
Protophenomenal <frontczakf@gmail.com>
2025-01-20 18:09:12 +0100

    Final refactored version

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ def get_book_text(path): #function to obtain a string of text from a path file
         return f.read()
 
 def word_counter(input_string): # function to count the words ommiting whitespace from an input string
-    words = input_string.split()
-    return len(words)
+    return len(input_string.split())
 
 def char_counter(input_string): # function to create a dictionary of the occurance of all characters in an input string (standardised to lowercase)
     char_dict = {}
     for char in input_string:
-        lowercase_char = char.lower()
-        if lowercase_char not in char_dict:
-            char_dict[lowercase_char] = 1
+        if char.lower() not in char_dict:
+            char_dict[char.lower()] = 1
         else:
-            char_dict[lowercase_char] += 1   
+            char_dict[char.lower()] += 1   
     return char_dict
 
 def report(path, text): # function to generate a formatted report using previous functions
-
     print(f"--- Begin report of {path} ---")
     print(f"{word_counter(text)} words found in the document")
     print()
-
-
-    base_char_dict = char_counter(text)
+    #base_char_dict = 
     char_dict_list = []
 
-    for char_key in base_char_dict:
-        dict_list_element = {"symbol":char_key,"number":base_char_dict[char_key]}
-        char_dict_list.append(dict_list_element)
+    for char_key in char_counter(text):
+        char_dict_list.append({"symbol":char_key,"number":char_counter(text)[char_key]})
 
     def sort_on(dict):
         return dict["number"]
@@ -38,14 +32,11 @@ def report(path, text): # function to generate a formatted report using previous
     for element in char_dict_list:
         if element["symbol"].isalpha():
             print(f"The '{element["symbol"]}' character was found {element["number"]} times") 
-
     print("--- End report ---")
     return
 
 def main():
     path = "books/frankenstein.txt"
-    text = get_book_text(path)
-    
-    return report(path,text)
+    return report(path,get_book_text(path))
 
 main()
